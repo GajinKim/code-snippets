f436ff287e16d8dfaf75876c44a450c590636e06
Gajin Kim <gajin2000@gmail.com>
2022-12-13 11:08:22 -0600

    added snippet

---
+++ first_nth_prime_numbers.js
@@ -0,0 +1,28 @@
+let nPrimes = 10;
+let primes = []; // 2 3 5 7 11
+let nonPrimes = [1]; // 1 4 6 8 9 10
+    
+function getNthPrimes(n) {
+    current = 2;
+    while (primes.length < n) {
+        isPrimeFlag = true;
+        for (let i = 2; i < current; i++) {
+            if (current % i == 0) {
+                isPrimeFlag = false;
+                break;
+            }
+        }
+        isPrimeFlag ? primes.push(current) : nonPrimes.push(current);
+        current++;
+    }
+}
+
+getNthPrimes(nPrimes);
+
+const sumOfFirstNthPrimes = primes.reduce((sum, number) => {
+    return sum += number;
+}, 0)
+
+console.log('primes:', primes.join(" "));
+console.log('non primes:', nonPrimes.join(" "));
+console.log('sum of first', nPrimes, 'prime numbers:', sumOfFirstNthPrimes);
